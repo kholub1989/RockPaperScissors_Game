0665268bf5963c71a74eacba7c508a09ac19020d
Krisztian Holub <kholub1989@gmail.com>
2019-07-16 11:39:42 -0400

    add RockPaperScissors_Game

---
+++ app.js
@@ -0,0 +1,119 @@
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
+
+  //Start the game
+  const startGame = () => {
+    const palyBtn = document.querySelector('.intro button');
+    const introScreen = document.querySelector('.intro');
+    const match = document.querySelector('.match');
+
+    palyBtn.addEventListener('click', () => {
+      introScreen.classList.add('fadeOut');
+      match.classList.replace('fadeOut', 'fadeIn');
+    });
+  };
+  //Play match
+  const playMatch = () => {
+    const options = document.querySelectorAll('.options button');
+    const playerHand = document.querySelector('.player-hand');
+    const computerHand = document.querySelector('.computer-hand');
+    const hands = document.querySelectorAll('.hands img');
+    hands.forEach(hand => {
+      hand.addEventListener('animationend', function () {
+        this.style.animation = '';
+      });
+    })
+
+    //Computer Options
+    const computerOptions = ['rock', 'paper', 'scissors'];
+
+    options.forEach((option) => {
+      option.addEventListener('click', function () {
+        //The computer choice
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+
+        setTimeout(() => {
+          //Here is where we call compare hends
+          compareHands(this.textContent, computerChoice);
+
+          //Update imagers
+          playerHand.src = `./assets/${this.textContent}.png`;
+          computerHand.src = `./assets/${computerChoice}.png`;
+        }, 2000);
+
+        //Animation
+        playerHand.style.animation = 'shakePlayer 2s ease';
+        computerHand.style.animation = 'shakeComputer 2s ease';
+      });
+    });
+  };
+
+  const updateScore = () => {
+    const playerScore = document.querySelector('.player-score p');
+    const computerScore = document.querySelector('.computer-score p');
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
+  }
+
+  const compareHands = (playerChoice, computerChoice) => {
+    //Update text
+    const winner = document.querySelector('.winner');
+    //Checking for a tie
+    if (playerChoice === computerChoice) {
+      winner.textContent = 'It is a tie';
+      return;
+    }
+    //Check for Rock
+    if (playerChoice === 'rock') {
+      if (computerChoice === 'scissors') {
+        winner.textContent = 'Player Wins';
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = 'Computer Wins';
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+    //Check for Paper
+    if (playerChoice === 'paper') {
+      if (computerChoice === 'scissors') {
+        winner.textContent = 'Computer Wins';
+        cScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = 'Player Wins';
+        pScore++;
+        updateScore();
+        return;
+      }
+    }
+    //Check for Scissors
+    if (playerChoice === 'scissors') {
+      if (computerChoice === 'rock') {
+        winner.textContent = 'Computer Wins';
+        cScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = 'Player Wins';
+        pScore++;
+        updateScore();
+        return;
+      }
+    }
+  }
+
+
+  // Is call all the inner fn
+  startGame();
+  playMatch();
+};
+
+//Start the game fn
+game();
